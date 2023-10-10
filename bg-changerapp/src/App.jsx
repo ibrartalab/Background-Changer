b55dd00145aaf9b0918645d3c8321b96b44e5215
IbrarKhan <ibrartalabkhan@gmail.com>
2023-10-10 10:25:53 -0700

    set bg-color and add card

--- bg-changerapp/src/App.jsx
+++ bg-changerapp/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-const [color, setColor] = useState("#ffffff")
+const [color, setColor] = useState("black")
 
   return (
-    <div className='w-full '>
-
+    <div className='w-screen h-screen duration-200'
+    style={{backgroundColor:color}}>
+   <div className='fixed flex flex-wrap bottom-12 inset-x-0 px-2 justify-center'>
+    <div className='flex flex-wrap justify-center gap-3 shadow-lg px-2 py-2 bg-white rounded-lg'>
+      test
+    </div>
+   </div>
     </div>
   )
 }
