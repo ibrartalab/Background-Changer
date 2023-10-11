614c818b1175862036f00ddcd296d165bdc53534
IbrarKhan <ibrartalabkhan@gmail.com>
2023-10-10 21:55:38 -0700

    Add ten cards and handle with useState.to update the state and UI as well.

--- bg-changerapp/src/App.jsx
+++ bg-changerapp/src/App.jsx
@@ -8,7 +8,56 @@ const [color, setColor] = useState("black")
     style={{backgroundColor:color}}>
    <div className='fixed flex flex-wrap bottom-12 inset-x-0 px-2 justify-center'>
     <div className='flex flex-wrap justify-center gap-3 shadow-lg px-2 py-2 bg-white rounded-lg'>
-      test
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-green-700'
+      style={{color:"white"}}
+      onClick={() => setColor("green")}>
+        Green
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-blue-700'
+      style={{color:"white"}}
+      onClick={() => setColor("blue")}>
+        blue
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button className='px-4 py-1 outline-none rounded-full bg-red-700'
+      style={{color:"white"}}
+      onClick={() => setColor("red")}>
+        Red
+      </button>
     </div>
    </div>
     </div>
@@ -16,3 +65,8 @@ const [color, setColor] = useState("black")
 }
 
 export default App
+
+
+
+
+
